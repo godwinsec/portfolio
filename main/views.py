265916e9554dc11e3689244d9cd6dff41bdb7ef6
Godwin <uchegodwin697@gmail.com>
2025-06-01 23:56:43 +0100

    Backend Commit
    Have updated files to have backend

---
+++ main/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import FileResponse
+import os
+from django.conf import settings
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'main/index.html')
+
+def about(request):
+    return render(request, 'main/about.html')
+
+def view_cv(request):
+    file_path = os.path.join(settings.STATICFILES_DIRS[0], 'files', 'GCV.pdf')
+    return FileResponse(open(file_path, 'rb'), content_type='application/pdf')

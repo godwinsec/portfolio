cf49df449361abd29b52af027d949b0353d304d7
Godwin <uchegodwin697@gmail.com>
2025-06-02 06:29:38 +0100

    Update allowed host

--- godwin/settings.py
+++ godwin/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-mt=@%n&2-7hn-p089$sxs^ab+g3l)56=&6&^0kvdeezc3c#8b)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app', 'www.godwin.ch', "godwin.ch" 'localhost', '127.0.0.1']
 
 
 # Application definition
